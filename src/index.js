f891bb6b1728ff05c6caef3067bf17a056da3203
ow-tech <wagwanwekon@outlook.com>
2022-06-29 14:53:02 +0400

    ignore node_modules

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const AuthAPI = require("./datasources/AuthKey")
 
 const resolvers = require("./resolvers")
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 
 const createTable = async () => {
